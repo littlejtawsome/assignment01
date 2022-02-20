d569f3c09da233e66fbb8c042b76c5751e05643b
Julie Shin <littlejt73@gmail.com>
2022-02-19 22:42:29 -0800

    second commit

---
+++ js/scripts.js
@@ -0,0 +1,154 @@
+//step1 이것도 테스트
+//step2
+//step3
+
+
+//1. 
+ //let someMonth; // snake casing to camel casing
+//function theMonth();//  snake casing to camel casing
+// let currentMonth // dash and  camel casing  
+//let summerMonth; // snake casing to camel casing
+//let myLibraryFunction; 
+//
+////2. 
+//4.5  //numeric literal
+//'Hello!' // string literal
+//false   //boolean literal
+//
+////3.
+//let anExpression = 3*(2/5)-8;
+//function checkNumbers(num1, num2) {
+//    if (num1 == 5 && num2 == 7) {
+//    alert('You guessed the right numbers!');
+//    }
+//    }
+//    checkNumbers(5, 10);
+//
+////4.
+//let  fName;
+//let  lName;
+//let  aDress;
+//let  cIty;
+//let  sTate;
+//let  zCode;
+//let  yourAge;
+//let  rSource;
+//let  mayWeContact;
+
+//5.
+//var fName;
+//fName = "Jeeyoung Julie "
+//var lName = "shin "
+//alert("your name is     "+ fName + lName);
+//var zCode = 92130;
+//alert ("Your zipcode is    "+ zCode)
+//
+//var yAge = prompt("Your age?");
+//alert (" yopur age is   " + yourAge);
+//
+////6. 
+//var qResult= prompt("What is your Quiz score?");
+//alert("Your Quiz score is   "+  qResult);
+//
+////7.  유형강제성
+//var women = true;
+//women = "Jane";
+//alert(women    + '  is a female!'); ///boolean + string = string
+
+ var num = 123;
+ var bool = true;
+ var numbool = num + bool; 
+alert(numbool);// number + boolean= number
+
+//8.
+//let someString = `Who once said, "Only two things are infinite, the universe 
+//and human stupidity, and I'm not sure about the former."`;
+//window.console.log(someString);
+
+//9.
+//null
+//let x = 100;
+//let y = null;
+//alert(x);
+//alert(y);
+//
+////undefined on
+//let x;
+//alert(x);
+//
+////10.
+//typeof "";  //string
+//typeof Infinity ;// number
+//typeof true; //boolean
+//typeof [1, 2, 4] ; //object
+//typeof undeclaredVariable ; //undefined
+//
+////11. 
+//let x = "Hello    ";
+//let y= prompt("your Name ?  ");
+//let z = "    Welcome to the JavaScript Class";
+//alert(x +   y    + z); 
+//
+////12. 
+//let a = "Jeeyoung Julie ";
+//let b = "Shin"
+//alert(a  +  b);
+
+//13.
+//let course = "JavaScript";
+//alert ("You learn " + course + " in this semester!");
+
+//14.
+//let x = "Hello ! ";
+//let y= "Jeeyoung Julie Shin";
+//let z = "\nWelcome to the JavaScript Class";
+//alert(x +   y    + z); 
+
+//15.
+//let x = "Hello    ";
+//let y= prompt("Your Name ?  ");
+//let z = "\nWelcome to the JavaScript Class";
+//alert(x +   y    + z); 
+
+//16. 
+//let x = "Hello I'm Julie ! ";
+//let z= prompt( "What is Your Class ?  ");
+//let y = " I'm taking a JavaScript Class\n";
+
+//alert(x +   y  +  "  and you are taking  a  " +z + "class!" ); 
+
+//17. 
+//var x = 10;
+//var y = 20;
+//console.log(x + y);
+
+//18.
+//let x = 20;
+//x = x +20;
+//console.log(x);
+
+//19.
+//let x = 20;
+//x = 5 * x;
+//console.log(x);
+
+//20.
+//var x = 20 % 3;
+//x = x / 1;
+//console.log(x);
+
+//21.
+//var a = 1;
+//var b = 2;
+//console.log(a <= 2);
+
+//22.
+
+//let a = 10;
+//let b = 20;
+//console.log(a >= b);
+
+
+
+
+
