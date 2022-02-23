9d4a914a32495b8799d39011f68c610129d6be44
Julie Shin <littlejt73@gmail.com>
2022-02-23 12:37:49 -0800

    0223_12:37pm

--- js/script.js
+++ js/script.js
@@ -1,166 +1,161 @@
-//step1 이것도 테스트
-//step2
-//step3
+
+//step 1. 
+        //let someMonth; // snake casing to camel casing
+        //function theMonth();//  snake casing to camel casing
+        //let currentMonth // dash and  camel casing  
+        //let summerMonth; // snake casing to camel casing
+        //let myLibraryFunction; 
+        //
+//step 2. 
+        //4.5  //numeric literal expression
+        //'Hello!' // string literal expressing
+        //false   //boolean literal expressing
+        //
+//step 3.
+        //let anExpression = 3*(2/5)-8;
+        //function checkNumbers(num1, num2) {
+        //    if (num1 == 5 && num2 == 7) {
+        //    alert('You guessed the right numbers!');
+        //    }
+        //    }
+        //    checkNumbers(5, 10);
+        //
+//step 4.
+        //let  fName;
+        //let  lName;
+        //let  aDress;
+        //let  cIty;
+        //let  sTate;
+        //let  zCode;
+        //let  yourAge;
+        //let  rSource;
+        //let  mayWeContact;
+
+//step 5.
+        //var myName;
+        //var isStudent;
+        //var lCity;
+        //myName = "Jeeyoung Julie Shin "
+        //lCity = "San Diego"
+        //isStudent= true;
+
+        //var myName = "Jeeyoung Julie Shin "
+        //var lCity = "San Diego"
+        //var sTudent = true;
+
+        //var myName = "Jeeyoung Julie Shin", var lCity= "San Diego", isStudent =true;
+
+//step 6. 
+        //var qSubject = "History"
+        //var qResult= 88;
+        //alert("Hi. You take a " + qSubject +"  test and your final Quiz score is  " + qResult);
+        //
+//step 7. coerced
+        //var women = true;
+        //women = "Jane";
+        //alert(women    + '  is a female!'); ///boolean + string = string
+
+        //var num = 123;
+        //var bool = true;
+        //var numbool = num + bool; 
+        //alert(numbool);
+        // number + boolean= number
+
+//step 8.
+        //let someString = `Who once said, \"Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.\"`;
+        //window.console.log(someString);
+
+//step 9.
+    
+
+        //var x =  null;
+        //window.console.log(x);  
+        
+        //var x ;
+        //window.console.log(x);
 
 
+//step 10.
+        //var age = 45;
+        //window.console.log(typeof age);
+        //window.console.log("The user's age is : " + age);
+        
+
+        //var nPie = parseFloat(3.14)
+        //window.console.log(nPie);
+
+        //var a = 1; 
+        //a.toString(a);
+        //console.log(a); //string
+
+        //var x= 5;
+        //var y= 10;
+        //console.log(x===5 && y ===10);
 
 
+//step 11. 
+        //let x = "Hello    ";
+        //let y= prompt("your Name ?  ");
+        //let z = "    Welcome to the JavaScript Class!";
+        //alert(x +   y + ","   + z); 
 
-//1. 
-//let someMonth; // snake casing to camel casing
-//function theMonth();//  snake casing to camel casing
-//let currentMonth // dash and  camel casing  
-//let summerMonth; // snake casing to camel casing
-//let myLibraryFunction; 
-//
-////2. 
-//4.5  //numeric literal expression
-//'Hello!' // string literal expressing
-//false   //boolean literal expressing
-//
-////3.
-//let anExpression = 3*(2/5)-8;
-//function checkNumbers(num1, num2) {
-//    if (num1 == 5 && num2 == 7) {
-//    alert('You guessed the right numbers!');
-//    }
-//    }
-//    checkNumbers(5, 10);
-//
-////4.
-//let  fName;
-//let  lName;
-//let  aDress;
-//let  cIty;
-//let  sTate;
-//let  zCode;
-//let  yourAge;
-//let  rSource;
-//let  mayWeContact;
+//step 12.  뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미
+        //var nAme = "Jeeyoung Julie Shin";
+        //alert("My name is  " + nAme);
 
-//5.
-//var myName;
-//var isStudent;
-//var lCity;
-//myName = "Jeeyoung Julie Shin "
-//lCity = "San Diego"
-//isStudent= true;
+ //step 13.
+        //let course = "JavaScript";
+        //alert ("You learn " + course + " in this semester!");
 
-//var myName = "Jeeyoung Julie Shin "
-//var lCity = "San Diego"
-//var sTudent = true;
+ //step 14.
+        //let x = "Hello ! ";
+        //let y= "Jeeyoung Julie Shin";
+        //let z = "\nWelcome to the JavaScript Class !";
+        //alert(x +   y    + z); 
 
-//var myName = "Jeeyoung Julie Shin", var lCity= "San Diego", isStudent =true;
+ //step 15.
+        //let x = "Hello  ";
+        //let y= prompt("Your Name ?  ");
+        //let z = "\nWelcome to the JavaScript Class";
+        //alert(x +   y    + z); 
 
-//6. 
-//var qSubject = "History"
-//var qResult= 88;
-//alert("Hi. You take a " + qSubject +"  test and your final Quiz score is  " + qResult);
-//
-////7. coerced
-//var women = true;
-//women = "Jane";
-//alert(women    + '  is a female!'); ///boolean + string = string
+//step 16. 
+        //let x = "Hello I'm Julie ! ";
+        //let z= prompt( "What is Your Class ?  ");
+        //let y = " I'm taking a JavaScript Class. \n";
+        //
+        //alert(x +   y  +  "You are taking  a  " +z + "  class!" ); 
 
- //var num = 123;
- //var bool = true;
- //var numbool = num + bool; 
-//alert(numbool);
-// number + boolean= number
+//step 17. 
+        //var x = 10;
+        //var y = 20;
+        //console.log(x + y);
 
-//8.
-//let someString = `Who once said, \"Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.\"`;
-//window.console.log(someString);
+//step 18.
+        //var x = 20;
+        //x = x +20;
+        //console.log(x);
 
-//9.
-//var a;
-//몰라몰라몰라몰라몰라몰라몰라몰라몰라몰라몰라몰라몰라몰라몰라몰라몰라몰라몰라몰
-//window.console.log(a+b);
+//step 19.
+        //var x = 20;
+        //x = 5 * x;
+        //console.log(x);
 
-//var num;
-//window.console.log(num);    //undifined
-//
-////10.
-//var age = 45;
-//window.console.log(typeof age);
-//window.console.log("The user's age is : " + age);
- 
+//step 20.
+        //var x = 20 % 3;
+        //x = x / 1;
+        //console.log(x);
 
-//var nPie = parseFloat(3.14)
- //window.console.log(nPie);
+//step 21.
+        //var a = 1;
+        //var b = 2;
+        //console.log(a <= 2);
 
-//var a = 1; 
-//a.toString(a);
-//console.log(a); //string
+//step 22.
 
-//var x= 5;
-//var y= 10;
-//console.log(x===5 && y ===10);
-
-
-////11. 
-//let x = "Hello    ";
-//let y= prompt("your Name ?  ");
-//let z = "    Welcome to the JavaScript Class!";
-//alert(x +   y + ","   + z); 
-
-////12.  뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미뭥미
-//var nAme = "Jeeyoung Julie Shin";
-//alert("My name is  " + nAme);
-
-//13.
-//let course = "JavaScript";
-//alert ("You learn " + course + " in this semester!");
-
-//14.
-//let x = "Hello ! ";
-//let y= "Jeeyoung Julie Shin";
-//let z = "\nWelcome to the JavaScript Class !";
-//alert(x +   y    + z); 
-
-//15.
-//let x = "Hello  ";
-//let y= prompt("Your Name ?  ");
-//let z = "\nWelcome to the JavaScript Class";
-//alert(x +   y    + z); 
-
-//16. 
-//let x = "Hello I'm Julie ! ";
-//let z= prompt( "What is Your Class ?  ");
-//let y = " I'm taking a JavaScript Class. \n";
-//
-//alert(x +   y  +  "You are taking  a  " +z + "  class!" ); 
-
-//17. 
-//var x = 10;
-//var y = 20;
-//console.log(x + y);
-
-//18.
-//var x = 20;
-//x = x +20;
-//console.log(x);
-
-//19.
-//var x = 20;
-//x = 5 * x;
-//console.log(x);
-
-//20.
-//var x = 20 % 3;
-//x = x / 1;
-//console.log(x);
-
-//21.
-//var a = 1;
-//var b = 2;
-//console.log(a <= 2);
-
-//22.
-
-//let a = 10;
-//let b = 20;
-//console.log(a+b <= b);
+        //let a = 10;
+        //let b = 20;
+        //console.log(a+b <= b);
 
 
 
